f330c23147c6e243b04d851e8fa85de695af41cf
Farhad <farhad9801@gmail.com>
2019-06-17 03:00:39 +0400

    todo controller

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,18 @@
+require File.expand_path("../../config/application.rb", File.dirname(__FILE__))
+
+class TodosController < Application::Controller
+  def new
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
